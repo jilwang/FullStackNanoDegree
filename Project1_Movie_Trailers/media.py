677877ff18b785748c3a1f454c05a3b4b0c65a2d
Ji Wang <jilwang@berkeley.edu>
2016-08-28 18:59:47 -0700

    Renamed directory

--- Project1_Movie_Trailers/media.py
+++ Project1_Movie_Trailers/media.py
@@ -5,7 +5,7 @@ class Movie:
         which contains the title and poster to display,
         and a link to the trailer on youtube
         """
-        
+
         self.title = title
         self.trailer_youtube_url = trailer_youtube_url
         self.poster_image_url = poster_image_url
