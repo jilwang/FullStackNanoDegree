037444b750039170f0b8a1375fc43cf18cd75c33
Ji Wang <jilwang@berkeley.edu>
2016-08-28 18:32:54 -0700

    Polished P1

--- Project 1 - Movie Trailers/media.py
+++ Project 1 - Movie Trailers/media.py
@@ -1,5 +1,11 @@
 class Movie:
     def __init__(self, title='', trailer_youtube_url='', poster_image_url=''):
+        """
+        This class represents a Movie in the navigation page,
+        which contains the title and poster to display,
+        and a link to the trailer on youtube
+        """
+        
         self.title = title
         self.trailer_youtube_url = trailer_youtube_url
         self.poster_image_url = poster_image_url
