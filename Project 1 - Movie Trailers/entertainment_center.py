f6b078ee65197a37ebc520297488b51e3262c4f9
Ji Wang <jilwang@berkeley.edu>
2016-08-28 18:34:07 -0700

    Polished P1

--- Project 1 - Movie Trailers/entertainment_center.py
+++ Project 1 - Movie Trailers/entertainment_center.py
@@ -8,18 +8,15 @@ def run():
     """
     zootopia = media.Movie("Zootopia",
                      "https://www.youtube.com/watch?v=jWM0ct-OLsM",
-                     "https://lumiere-a.akamaihd.net/v1/images/movie_poster_"
-                     "zootopia_866a1bf2.jpeg?region=0%2C0%2C300%2C450")
+                     "https://lumiere-a.akamaihd.net/v1/images/movie_poster_zootopia_866a1bf2.jpeg?region=0%2C0%2C300%2C450")  # noqa
 
     suicide = media.Movie("Suicide Squad",
                     "https://www.youtube.com/watch?v=CmRih_VtVAs",
-                    "http://cdn.collider.com/wp-content/uploads/2016/06/"
-                    "suicide-squad-poster-400x600.jpg")
+                    "http://cdn.collider.com/wp-content/uploads/2016/06/suicide-squad-poster-400x600.jpg")  # noqa
 
     jurassic = media.Movie("Jurassic Park 1",
                      "https://www.youtube.com/watch?v=lc0UehYemQA",
-                     "https://upload.wikimedia.org/wikipedia/en/e/e7/"
-                     "Jurassic_Park_poster.jpg")
+                     "https://upload.wikimedia.org/wikipedia/en/e/e7/Jurassic_Park_poster.jpg")  # noqa
 
     move_list = [zootopia, suicide, jurassic]
     fresh_tomatoes.open_movies_page(move_list)
